9885c1559f6e8b045b61ca4f4e1ac3faaa778e1a
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-25 21:26:39 +0000

    moved connect to client

--- client.js
+++ client.js
@@ -1,18 +1,18 @@
-
 const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
 const connect = function() {
-  const conn = net.createConnection({
+  const conn = net.createConnection({ 
     host: 'localhost',
     port: 50541
   });
-  conn.on('connect', () => {
-    conn.write('Hello from client!');
-  });
   // interpret incoming data as text
-  conn.setEncoding('utf8');
-
+  conn.setEncoding('utf8'); 
+  conn.on('connect', () => {
+    conn.write('you ded cuz you idled');
+  });
   return conn;
 }
-
-console.log('Connecting ...');
-connect();
+module.exports = connect;
