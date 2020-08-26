74f2435a5fec551657d4121a7b642a9d713b99d2
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-26 03:46:55 +0000

    add Name : SWN

--- client.js
+++ client.js
@@ -1,18 +1,25 @@
 const net = require('net');
+const { Server } = require('http');
 
 /**
  * Establishes connection with the game server
  */
 const connect = function() {
   const conn = net.createConnection({ 
-    host: 'localhost',
+    host: '10.0.2.15',
     port: 50541
+  });
+  conn.on('connect', () => {
+    console.log('the connection is successfully established');
+    conn.write('Name: SWN');
+  });
+  
+  conn.on('end', () => {
+    console.log('you ded cuz you idled');
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('connect', () => {
-    conn.write('you ded cuz you idled');
-  });
+  
   return conn;
 }
 module.exports = connect;
