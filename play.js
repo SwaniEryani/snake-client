c727ed66a1ed08896073a243c7f89d20f825ce7d
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-26 05:31:44 +0000

    add input

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
+const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-
-const connect = require('./client');
 console.log('Connecting ...');
 connect();
-const handleUserInput = function (stdin) {
-  stdin.on('data', (key) => {
-    if(key === '\u0003'){
-      process.exit();
-    }
-  })
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
-handleUserInput(setupInput());
+
+setupInput();
