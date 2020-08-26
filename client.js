0b225c27f36a3a1013638ca768ee66367eaa3cd0
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-26 05:10:40 +0000

    establish our UI

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
 const net = require('net');
 const { Server } = require('http');
-const moves = [ "Move: down", "Move: up", "Move: left", "Move: right"];
+const moves = ["Move: down", "Move: up", "Move: left", "Move: right"];
 /**
  * Establishes connection with the game server
  */
@@ -12,29 +12,34 @@ const connect = function () {
   conn.on('connect', () => {
     console.log('the connection is successfully established');
     conn.write('Name: SWN');
-    setInterval( () =>{
-      let rand = Math.floor(Math.random() * (moves.length + 1));
-      conn.write(`${moves[rand]}`)
-    }, 50 );
-    
+    conn.write(`Move: up `)
   });
+    // conn.on('connect', () => {
+    //   console.log('the connection is successfully established');
+    //   conn.write('Name: SWN');
+    //   setInterval( () =>{
+    //    // let rand = Math.floor(Math.random() * (moves.length + 1));
+    //     conn.write(`/*${moves[rand]}*/Move: up `)
+    //   }, 50 );
+
+    // });
+
+    // for (let i = 0; i < moves.length; i++) {
+    //   setTimeout(() => {
+    //     let rand = Math.floor(Math.random() * (moves.length + 1));
+    //     conn.write(`${moves[rand]}`);
+    //     console.log(`${moves[rand]}`);
+    //   }, (i+1) * 50);
+    // }
+
+
+    conn.on('end', () => {
+      console.log('you ded cuz you idled');
+    });
   
-  // for (let i = 0; i < moves.length; i++) {
-  //   setTimeout(() => {
-  //     let rand = Math.floor(Math.random() * (moves.length + 1));
-  //     conn.write(`${moves[rand]}`);
-  //     console.log(`${moves[rand]}`);
-  //   }, (i+1) * 50);
-  // }
-  
+    // interpret incoming data as text
+    conn.setEncoding('utf8');
 
-  conn.on('end', () => {
-    console.log('you ded cuz you idled');
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-
-  return conn;
-}
+    return conn;
+  }
 module.exports = connect;
--- play.js
+++ play.js
@@ -3,3 +3,18 @@
 const connect = require('./client');
 console.log('Connecting ...');
 connect();
+const handleUserInput = function (stdin) {
+  stdin.on('data', (key) => {
+    if(key === '\u0003'){
+      process.exit();
+    }
+  })
+}
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+handleUserInput(setupInput());
