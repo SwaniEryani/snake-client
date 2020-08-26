a0bad41e571489cce2a6e88d4d12a0ba6e1c5f4b
SwaniEryani <aleryanisilwan@gmail.com>
2020-08-26 04:44:54 +0000

    Moves

--- client.js
+++ client.js
@@ -1,25 +1,40 @@
 const net = require('net');
 const { Server } = require('http');
-
+const moves = [ "Move: down", "Move: up", "Move: left", "Move: right"];
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: '10.0.2.15',
     port: 50541
   });
   conn.on('connect', () => {
     console.log('the connection is successfully established');
     conn.write('Name: SWN');
+    setInterval( () =>{
+      let rand = Math.floor(Math.random() * (moves.length + 1));
+      conn.write(`${moves[rand]}`)
+    }, 50 );
+    
   });
   
+  // for (let i = 0; i < moves.length; i++) {
+  //   setTimeout(() => {
+  //     let rand = Math.floor(Math.random() * (moves.length + 1));
+  //     conn.write(`${moves[rand]}`);
+  //     console.log(`${moves[rand]}`);
+  //   }, (i+1) * 50);
+  // }
+  
+
   conn.on('end', () => {
     console.log('you ded cuz you idled');
   });
+
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
+  conn.setEncoding('utf8');
+
   return conn;
 }
 module.exports = connect;
